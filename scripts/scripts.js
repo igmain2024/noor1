43fa67238cac001a5a7c4d9074d9c910b9559b29
quakeroatsgod <agnellogriffen@gmail.com>
2022-06-13 16:12:19 -0700

    Blog posts! :D

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,9 +2,9 @@
 //     window.history.pushState("http://localhost:8888/", "");
 //     window.location.replace("html/new.html")
 // }
-/*This toggles the contents appearing in the dropdown menu*/
-function toggleDropdown() {
-    document.getElementById("blogEntries").classList.toggle("showContents");
+/*This toggles the contents appearing in the dropdown menu based on an ID*/
+function toggleDropdown(ID) {
+    document.getElementById(ID).classList.toggle("showContents");
 }
 /*This disables showing the dropdown menu contents if clicking off the menu*/
 function disableDropdownOOB(){
